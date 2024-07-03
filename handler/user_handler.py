a10ee4eaa723669e35374dcfcf540efdd7f96322
qsef3546 <qsef354610@gmail.com>
2024-07-03 19:09:47 +0900

    회원 정보 수정및 코드 분리

--- handler/user_handler.py
+++ handler/user_handler.py
@@ -1,7 +1,39 @@
 from fastapi import APIRouter
-from model.sqlconnect import user,insert,selects,update
-
+from fastapi.responses import JSONResponse
+from model.sqlconnect import user,insert,selects,select_one,update
+from error_code import error
+import re
+import hashlib
 user_router = APIRouter(prefix="/user")
+pw_hash = hashlib.sha256()
+
+def handle_error(res,status_code=200):
+    return JSONResponse({"code": res, "message": error.errorcode[res]}, status_code)
+
+def emptycheck(u:user):
+    if not u.email:
+        return 1101
+    if not u.name:
+        return 1102
+    if not u.pw:
+        return 1103
+    return 200
+
+def email_validation(email:str):
+    if not re.match(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',email):
+        return  1104
+    return 200
+
+def password_validation(pw:str):
+    if len(pw) < 8:
+        return 1105
+    if not re.match(r'(?=.*?[a-z])',pw):
+        return 1106
+    if not re.match(r'(?=.*?[A-Z])',pw):
+        return 1107
+    if not re.match(r'(?=.*?[#?!@$%^&*-])',pw):
+        return 1108
+    return 200
 
 
 @user_router.get("/list")
@@ -10,9 +42,37 @@ def user_select():
 
 @user_router.post("/insert")
 def user_insert(u:user):
-    return insert(u)
+    if (res := emptycheck(u))  != 200:
+        return handle_error(res)
+    elif (res := email_validation(u.email)) != 200 :
+        return handle_error(res)
+    elif (res := password_validation(u.pw)) != 200:
+        return handle_error(res)
+    
+    if select_one(u.email) :
+        return handle_error(1201,409)
+                
+    pw_hash.update(u.pw.encode())
+    u.pw = pw_hash.hexdigest()
+    if (res := insert(u)) == False:
+        handle_error(1200,500)        
+    else:
+        return JSONResponse({"message":f"{u.email} 님의 회원가입이 완료되었습니다."},200)
     
 @user_router.patch("/put")
 def user_put(u:user):
-    res = update(u)
-    return {"message": "update success!"}
+    putuser = select_one(u.email)
+    if not putuser:
+        return handle_error(1202)
+            
+    if u.name and putuser.name != u.name:
+        putuser.name = u.name
+    if u.pw:
+        if (res := password_validation(u.pw)) != 200:
+            return handle_error(res)
+        pw_hash.update(u.pw.encode())
+        putuser.pw = pw_hash.hexdigest()
+    if (res := update(putuser)) == False:
+        handle_error(1200,500)        
+    else:
+        return JSONResponse({"message":"수정이 완료되었습니다."},200)
--- model/sqlconnect.py
+++ model/sqlconnect.py
@@ -1,43 +1,14 @@
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 from model.sqlconf import config
 from error_code import error
-from fastapi.responses import JSONResponse
-import hashlib
-import re
+from inspect import currentframe, getframeinfo
 engine = create_engine(str(config.SQLALCHEMY_DATABASE_URI))
 SQLModel.metadata.create_all(engine)
-pw_hash = hashlib.sha256()
 
 class user(SQLModel, table=True):
     email: str = Field(unique=True,nullable=False,primary_key=True)
     name: str = Field(nullable=False)
     pw: str = Field(nullable=False)
-
-def emptycheck(u:user):
-    if not u.email:
-        return 1101
-    if not u.name:
-        return 1102
-    if not u.pw:
-        return 1103
-    return 200
-
-def email_validation(email:str):
-    if not re.match(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',email):
-        return  1104
-    return 200
-
-def password_validation(pw:str):
-    if len(pw) < 8:
-        return 1105
-    if not re.match(r'(?=.*?[a-z])',pw):
-        return 1106
-    if not re.match(r'(?=.*?[A-Z])',pw):
-        return 1107
-    if not re.match(r'(?=.*?[#?!@$%^&*-])',pw):
-        return 1108
-    return 200
-
     
 def selects():
     with Session(engine) as session:
@@ -45,34 +16,22 @@ def selects():
         res = session.exec(statement).all()
         s = {'res':[x.__dict__ for x in res]}
         return s
+    
 def select_one(email:str):
-    with Session(engine) as session:
-        statement = select(user).where(user.email==email)
-        userinfo = session.exec(statement).one_or_none()
-        if not userinfo:
-            return None
-        else:
-            return userinfo
-def insert(u:user):
-    res = emptycheck(u) 
-    if res != 200:
-        return JSONResponse({"code":res,"message":error.errorcode[res]},200)
-    
-    res = email_validation(u.email)
-    if res != 200 :
-        return JSONResponse({"code":res,"message":error.errorcode[res]},200)
-    
-    res = password_validation(u.pw)
-    if res != 200:
-        return JSONResponse({"code":res,"message":error.errorcode[res]},200)
-    
     try:
-        res = select_one(u.email)
-        if res :
-            return JSONResponse({"code":1109,"message":error.errorcode[1109].format(u.email)},200)
-            
-        pw_hash.update(u.pw.encode())
-        u.pw = pw_hash.hexdigest()
+        with Session(engine) as session:
+            statement = select(user).where(user.email==email)
+            userinfo = session.exec(statement).one_or_none()
+            print(f'[DB] [SUCCESS] file : {__file__} , function : select_one')
+            return userinfo
+    except Exception as e:
+        print(f'[DB] [ERROR] file : {__file__} , function : insert , message : {e}')
+        return False
+    
+def insert(u:user):
+    res = True
+    error_msg = ''
+    try:
         with Session(engine) as session:
             try:
                 session.add(u)
@@ -82,24 +41,30 @@ def insert(u:user):
                 session.rollback()
                 raise e
     except Exception as e:
-        return JSONResponse({"code":1200,"message":error.errorcode[1200],"error message":f'{e}'},500)
-    
-    return JSONResponse({"message":f"{u.email} 님의 회원가입이 완료되었습니다."},200)
+        res = False
+        error_msg = e
+    finally:
+        if res:
+            print(f'[DB] [SUCCESS] file : {__file__} , function : insert')
+        else :
+            print(f'[DB] [ERROR] file : {__file__} , function : insert , message : {error_msg}')
+        return res
 
 
 
 def update(u:user):
-    with Session(engine) as session:
-        statement = select(user).where(user.id == u.id)
-        putuser = session.exec(statement).one()
-        if not putuser:
-            return {"message":"id is not exist"}
-        if u.pw is not None:
-            putuser.pw = u.pw
-        if putuser.name != u.name:
-            putuser.name = u.name
-        if (putuser.age != u.age) and u.age != None:
-            putuser.age = u.age
-        session.add(putuser)
-        session.commit()
-
+    res = True
+    error_msg = ''
+    try:
+        with Session(engine) as session:
+            session.add(u)
+            session.commit()
+    except Exception as e:
+        res = False
+        error_msg = e
+    finally:
+        if res:
+            print(f'[DB] [SUCCESS] file : {__file__} , function : insert')
+        else :
+            print(f'[DB] [ERROR] file : {__file__} , function : insert , message : {error_msg}')
+        return res
